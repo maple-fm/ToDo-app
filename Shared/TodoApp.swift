11e5b826373eb51f739edb7d4827f1d651e4387e
fu-ma <fuma0205boo@gmail.com>
2022-06-09 23:58:35 +0900

    Initial Commit

---
+++ Shared/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Shared
+//
+//  Created by 出口楓真 on 2022/06/09.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
