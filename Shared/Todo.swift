972191f074713f5fab0ba38e3a6f590363649a1c
maple-fm <fuma0205boo@gmail.com>
2022-07-04 20:30:32 +0900

    テキストの入力チェックとstructの生成

---
+++ Shared/Todo.swift
@@ -0,0 +1,15 @@
+//
+//  Todo.swift
+//  Todo
+//
+//  Created by 出口楓真 on 2022/07/04.
+//
+
+import Foundation
+
+struct Todo{
+    var name: String
+    var deadline: String
+//    var category: String
+    var description: String
+}
