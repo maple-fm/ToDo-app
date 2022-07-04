1be5bbb70acbf67058605cd73d339d9f4bb34c57
maple-fm <fuma0205boo@gmail.com>
2022-07-04 20:29:08 +0900

    ボタンの生成とTodo structの作成

--- Shared/NewCreateView.swift
+++ Shared/NewCreateView.swift
@@ -8,32 +8,115 @@
 import SwiftUI
 
 struct NewCreateView: View {
-    @State private var create = false
+    @State private var close = false
+//    @State private var aTask = ""
+//    @State private var Date = ""
+    @State private var aCategory = UIColor.red
+//    @State private var description = ""
+    @State var todo: Todo
+    @Environment(\.dismiss) var dismiss
+    
+
     var body: some View {
-        NavigationView{
+        ScrollView{
             VStack{
-                Image("Normal")
-                Button(action:{
-                    self.create.toggle()
-                }){
-                    Image("Button")
+                HStack(spacing: 97.51){
+                    Text("新規作成")
+                        .font(.title2)
+                        .fontWeight(.bold)
+
+                    Button("×"){
+                        
+                        dismiss()
+
+                    }
+                    .foregroundColor(Color.blue)
+                    .font(.largeTitle)
+                    
+                    
+                }
+                .frame(alignment: .top)
+                .padding(EdgeInsets(
+                    top: 900-UIScreen.main.bounds.size.height,
+                    leading:500-UIScreen.main.bounds.size.width,
+                    bottom:0,
+                    trailing: 0))
+                Divider()
+           
+                Group{
+                    
+                    Text("タスク名")
+                        .padding(.top, 33)
+                    TextField("20文字以内で入力してください", text: $todo.name)
+                        .frame(width: 326, height: 60)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        
+
+                    Text("期日")
+                        .padding(.top, 33)
+                    TextField("年/月/日", text: $todo.deadline)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    
+                    Text("カテゴリー")
+                        .padding(.top, 33)
+                
+                    NavigationView{
+                        Form{
+                            Picker("選択してください", selection: $aCategory){
+                                Text("仕事").tag(UIColor.red)
+                                Text("勉強").tag(UIColor.blue)
+                                Text("遊び").tag(UIColor.yellow)
+                                Text("その他").tag(UIColor.gray)
+                            }
+                        }
+                    }
+
+                    
+                    
+                    
+                    Text("詳細")
+                        .padding(.top, 33)
+                    TextField("詳細", text: $todo.description)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding(.bottom, 155)
+                    
+                    
+                }
+                .frame(width: 326, alignment: .leading)
+    //            .padding(EdgeInsets(
+    //                top:0,
+    //                leading: 0,
+    //                bottom:33,
+    //                trailing: 0
+    //            ))
+            
+
+                
+                Section{
+                    Button(action:{}){
+                        Text("作成する")
+                            .frame(width: 310, height: 60)
+                    }
+                    .frame(width: 342, height: 60)
+                    .foregroundColor(todo.name.isEmpty ? Color.black : Color.white)
+                    .background(todo.name.isEmpty ? Color.gray : Color.blue)
+                    .disabled(todo.name.isEmpty)
+
+                    .cornerRadius(15)
+                    
                 }
                 
-            }
-            .navigationTitle("新規作成")
-                        
-            
-    
-            
-        }
-        .frame(alignment: .leading)
+//                .background(Color.blue)
 
+                
+            }
         
+        }
     }
 }
 
 struct NewCreateView_Previews: PreviewProvider {
     static var previews: some View {
-        NewCreateView()
+        NewCreateView(todo: Todo(name: "", deadline: "", description: ""))
     }
 }
